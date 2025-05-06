7ec1a5cfcf2b5e90f6f7f172995d8f03323c8e97
Munna-Scripts <munnascripts@gmail.com>
2025-05-06 17:37:46 +0600

    Aos Added And Fixed Some Structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+// Aos Animation ------------------
+AOS.init();
+// Mouse Follower circle------------
 const circleElement = document.querySelector('.circle');
 
 const mouse = {
@@ -16,24 +19,18 @@ window.addEventListener('mousemove', e => {
 
 // Speed factor
 // Between 0 and 1 (0 = smoother, 1 = instant)
-const speed = 0.15;
+const speed = 0.05;
 
 const tick = () => {
-  // (mouse.x - circle.x) calculates the difference between the current x-coordinate of the mouse and the current x-coordinate of the circle.
-  // (mouse.x - circle.x) * speed multiplies the difference by the speed factor, which determines how quickly the circle should move towards the mouse position
   circle.x += (mouse.x - circle.x) * speed;
   circle.y += (mouse.y - circle.y) * speed;
-
-  // Update circle element's position
   circleElement.style.transform = `translate(${circle.x}px, ${circle.y}px)`;
-
-  // Call function on next frame
   window.requestAnimationFrame(tick);
 }
 
 tick();
 
-//------------------- Slider---------
+//Food Slider--------------------
 $('.counter').counterUp({
   delay: 10,
   time: 1000
